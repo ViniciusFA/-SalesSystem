087d5d462cdbbf2dd3b047f21c7c9d703a8479c6
Vinicius Fontes <vinicius.fontesdeandrade@gmail.com>
2020-09-08 16:55:27 -0300

    Adding new ViewModel Produto and Venda

---
+++ SistemaVenda/Models/ViewModel/ProdutoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Models.ViewModel
+{
+    public class ProdutoViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public double Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Range(0.1, Double.PositiveInfinity)]
+        public decimal? Valor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int? CodigoCategoria { get; set; }
+        public IEnumerable<SelectListItem> ListaCategorias { get; set; }
+    }
+}
---
+++ SistemaVenda/Models/ViewModel/VendaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Models.ViewModel
+{
+    public class VendaViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public DateTime? Data { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int? CodigoCliente { get; set; }
+
+        public IEnumerable<SelectListItem> ListaClientes { get; set; }
+
+        public IEnumerable<SelectListItem> ListaProdutos{ get; set; }
+
+        public string JsonProdutos { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
