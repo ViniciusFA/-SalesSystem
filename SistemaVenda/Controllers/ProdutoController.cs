dd2b2ed610a5de24d6019cddcd6ed55ce1311509
Vinicius Fontes <vinicius.fontesdeandrade@gmail.com>
2020-09-08 16:56:52 -0300

    Implementing Controllers Produto and Venda

---
+++ SistemaVenda/Controllers/ProdutoController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using SistemaVenda.Models.ViewModel;
+
+namespace SistemaVenda.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public ProdutoController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Produto> listaProduto = mContext.Produto.Include(x => x.Categoria).ToList();
+            return View(listaProduto);
+        }
+
+        private IEnumerable<SelectListItem> ListaCategoria()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>();
+
+            lista.Add(new SelectListItem()
+            {
+                Value = string.Empty,
+                Text = "Selecione"
+            });
+
+            foreach (var item in mContext.Categoria.ToList())
+            {
+                lista.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Descricao.ToString()
+                });
+            }
+
+            return lista;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            ProdutoViewModel viewModel = new ProdutoViewModel();
+            viewModel.ListaCategorias = ListaCategoria();
+
+            if (id.HasValue)
+            {
+                var categoriaDb = mContext.Produto.Where(x => x.Codigo == id).SingleOrDefault();
+
+                viewModel.Codigo = categoriaDb.Codigo;
+                viewModel.Descricao = categoriaDb.Descricao;
+                viewModel.Quantidade = categoriaDb.Quantidade;
+                viewModel.Valor = categoriaDb.Valor;
+                viewModel.CodigoCategoria = categoriaDb.CodigoCategoria;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ProdutoViewModel viewModel)
+        {
+            //ModelState check the model's DataAnnotations    
+            if (ModelState.IsValid)
+            {
+                Produto produto = new Produto
+                {
+                    Codigo = viewModel.Codigo,
+                    Descricao = viewModel.Descricao,
+                    Quantidade = viewModel.Quantidade,
+                    Valor = viewModel.Valor.Value,
+                    CodigoCategoria = viewModel.CodigoCategoria.Value
+                };
+
+                if (viewModel.Codigo != null)
+                    mContext.Entry(produto).State = EntityState.Modified;
+                else
+                    mContext.Produto.Add(produto);
+
+                mContext.SaveChanges();
+            }
+            else
+            {
+                viewModel.ListaCategorias = ListaCategoria();
+                return View(viewModel);
+            }              
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            Produto resultProduto = null;
+
+            if (id.HasValue)
+            {
+                resultProduto = mContext.Produto.Where(x => x.Codigo == id).SingleOrDefault();
+                mContext.Produto.Remove(resultProduto);
+                mContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ SistemaVenda/Controllers/VendaController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using SistemaVenda.Models.ViewModel;
+
+namespace SistemaVenda.Controllers
+{
+    public class VendaController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public VendaController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Venda> listaProduto = mContext.Venda.ToList();
+            return View(listaProduto);
+        }
+
+        private IEnumerable<SelectListItem> ListaCliente()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>
+            {
+                new SelectListItem()
+                {
+                    Value = string.Empty,
+                    Text = "Selecione"
+                }
+            };
+
+            foreach (var item in mContext.Cliente.ToList())
+            {
+                lista.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Nome.ToString()
+                });
+            }
+
+            return lista;
+        }
+
+        private IEnumerable<SelectListItem> ListaProduto()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>
+            {
+                new SelectListItem()
+                {
+                    Value = string.Empty,
+                    Text = "Selecione"
+                }
+            };
+
+            foreach (var item in mContext.Produto.ToList())
+            {
+                lista.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Descricao.ToString()
+                });
+            }
+
+            return lista;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            VendaViewModel viewModel = new VendaViewModel();
+            viewModel.ListaClientes = ListaCliente();
+            viewModel.ListaProdutos = ListaProduto();
+
+            if (id.HasValue)
+            {
+                var categoriaDb = mContext.Venda.Where(x => x.Codigo == id).SingleOrDefault();
+
+                viewModel.Codigo = categoriaDb.Codigo;
+                viewModel.Data = categoriaDb.Data;
+                viewModel.CodigoCliente = categoriaDb.CodigoCliente;
+                viewModel.Total = categoriaDb.Total;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(VendaViewModel viewModel)
+        {
+            //ModelState check the model's DataAnnotations    
+            if (ModelState.IsValid)
+            {
+                Venda venda = new Venda
+                {
+                    Codigo = viewModel.Codigo,
+                    Data = viewModel.Data.Value,
+                    CodigoCliente = viewModel.CodigoCliente.Value,
+                    Total = viewModel.Total,
+                    Produtos = JsonConvert.DeserializeObject<ICollection<VendaProdutos>>(viewModel.JsonProdutos)
+                };
+
+                if (viewModel.Codigo != null)
+                    mContext.Entry(venda).State = EntityState.Modified;
+                else
+                    mContext.Venda.Add(venda);
+
+                mContext.SaveChanges();
+            }
+            else
+            {
+                viewModel.ListaClientes = ListaCliente();
+                viewModel.ListaProdutos = ListaProduto();
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            Venda resultVenda = null;
+
+            if (id.HasValue)
+            {
+                resultVenda = mContext.Venda.Where(x => x.Codigo == id).SingleOrDefault();
+                mContext.Venda.Remove(resultVenda);
+                mContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet("LerValorProduto/{CodigoProduto}")]
+        public decimal LerValorProduto(int CodigoProduto)
+        {
+            return mContext.Produto.Where(x => x.Codigo == CodigoProduto).Select(x => x.Valor).SingleOrDefault();
+        }
+    }
+}
