bddd1368e0015259c7fea0e1d797977f66218bb6
Vinicius Fontes <vinicius.fontesdeandrade@gmail.com>
2020-09-08 21:09:27 -0300

    Creating and implementing RelatorioController

---
+++ SistemaVenda/Controllers/RelatorioController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVenda.DAL;
+using SistemaVenda.Models.ViewModel;
+
+namespace SistemaVenda.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public RelatorioController(ApplicationDbContext Context)
+        {
+            mContext = Context;
+        }
+
+        public IActionResult Grafico()
+        {
+            var lista = mContext.VendaProdutos
+                .GroupBy(x => x.CodigoProduto)
+                .Select(y => new GraficoViewModel
+                {
+                    CodigoProduto = y.FirstOrDefault().CodigoProduto,
+                    Descricao = y.FirstOrDefault().Produto.Descricao,
+                    TotalVendido = y.Sum(x => x.Quantidade)
+                }).ToList();
+
+            string valores = string.Empty;
+            string labels = string.Empty;
+            string cores = string.Empty;
+
+            var random = new Random();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                valores += lista[i].TotalVendido.ToString() + " , ";
+                labels += "'" + lista[i].Descricao + "' , ";
+                cores += "'" + String.Format("#{0:X6}", random.Next(0x10000000)) + "' , ";
+            }
+
+            ViewBag.Valores = valores;
+            ViewBag.Labels = labels;
+            ViewBag.Cores = cores;
+
+            return View(lista);
+           
+        }
+    }
+}
